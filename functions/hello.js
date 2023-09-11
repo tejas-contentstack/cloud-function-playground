709f42911173fe4b9ce6b8df7b1266a81533d471
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2023-09-11 16:20:39 +0530

    Update hello.js

--- functions/hello.js
+++ functions/hello.js
@@ -1,5 +1,5 @@
 // functions/hello.js
 
 export default function handler(request, response) {
-    return response.status(200).json({message: "testing"});
+    return response.status(200).json({message: "testing 123"});
   }
