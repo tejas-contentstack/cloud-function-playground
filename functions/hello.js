6c22972ba96f911ac2800919e7e6ce514c907ca5
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-05-31 15:19:25 +0530

    fix request logging

--- functions/hello.js
+++ functions/hello.js
@@ -6,8 +6,8 @@ export default function handler(request, response) {
     console.debug({obj: true});
     console.error({obj: true});
     console.warn({obj: true});
-    console.log(req);
-    console.log(req.headers);
-    console.log(req.body)
+    console.log(request);
+    console.log(request.headers);
+    console.log(request.body)
     return response.status(200).json({message: "testing 123"});
   }
