d2aaf304c5f7be184ec9d019533a5d674f6aa288
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-06-05 17:33:32 +0530

    log undefined and null

--- functions/hello.js
+++ functions/hello.js
@@ -4,5 +4,7 @@ export default function handler(request, response) {
     console.log("Hello");
     console.log({obj: true});
     console.log(request.headers);
+    console.log(undefined)
+    console.log(null)
     return response.status(200).json({message: "testing 123"});
   }
