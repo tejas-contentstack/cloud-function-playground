fc0145053e0cee1e7e9d7d704aad4613fe4a3ffa
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-12-04 01:04:35 +0530

    Update hi.js

--- functions/user-sessions/hi.js
+++ functions/user-sessions/hi.js
@@ -5,5 +5,5 @@ export default function handler(request, response) {
     console.log(request.headers);
     console.log(undefined)
     console.log(null)
-    res.status(200).send("Success");
+    return res.status(200).end();
   }
