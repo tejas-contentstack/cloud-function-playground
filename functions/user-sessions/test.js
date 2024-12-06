060e1da42152059103b940170d97af79d0067c55
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-12-06 15:27:12 +0530

    Create test.js

---
+++ functions/user-sessions/test.js
@@ -0,0 +1,9 @@
+
+export default function handler(request, response) {
+    console.log("Hello");
+    console.log({obj: true});
+    console.log(request.headers);
+    console.log(undefined)
+    console.log(null)
+    return response.status(200).headers({"contentType": "application/json"}).end("hello");
+  }
