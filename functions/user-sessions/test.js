b46fbc530e17f884484fbc670b1d922f0bb3cc7c
tejas-contentstack <132910969+tejas-contentstack@users.noreply.github.com>
2024-12-06 16:30:37 +0530

    Update test.js

--- functions/user-sessions/test.js
+++ functions/user-sessions/test.js
@@ -1,11 +1,6 @@
 
 export default function handler(request, response) {
-    console.log("Hello");
-    console.log({obj: true});
-    console.log(request.headers);
-    console.log(undefined)
-    console.log(null)
-    return     response
+    return response
         .writeHead(200, { "Content-Type": "application/json" })
         .end(JSON.stringify({ headers: { "content-type": "application/json" }, body: "hello" }));
 
